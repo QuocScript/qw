a85b507b349988a8729c9f23c00555f580dc991e
Kien Quoc <daniel@192.168.1.4>
2023-10-03 17:45:30 +0700

    updated

--- obj.cs
+++ obj.cs
@@ -1,15 +1,3 @@
-/// create - INIT
-state = "idle";
-moveX = 0;
-moveY = 0;
-grav = 0.69;
-spd_normal = 3;
-spd_run = 5;
-spd_roll = 10;
-
-//ALLPY
-my_spd = spd_normal;
-
 /// step - STATE CONTROLLER
 // 1. input
 inputW = keyboard_check_pressed(ord("W"));
