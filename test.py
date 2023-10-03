7fccc66c5a815499161b8441d40d3f747585449f
Kien Quoc <daniel@192.168.1.4>
2023-10-03 12:34:04 +0700

    test project

---
+++ test.py
@@ -0,0 +1,21 @@
+input_str = int(input('hãy nhập số vào đây để tôi kiểm tra chẵn lẻ: '))
+
+if input_str.isdigit():
+    num = int(input_str)
+    if num % 2 == 0:
+        print("Số bạn nhập là số chẵn")
+    else:
+        print("Số bạn nhập là số lẻ")
+else:
+    print("Đây không phải là một số nguyên.")
+
+input_str = input('hãy nhập số vào đây để tôi kiểm tra chẵn lẻ: ')
+
+if input_str.isdigit():
+    num = int(input_str)
+    if num % 2 == 0:
+        print("Số bạn nhập là số chẵn")
+    else:
+        print("Số bạn nhập là số lẻ")
+else:
+    print("Đây không phải là một số nguyên.")
